3898f98ee76dd6362471fa7c4be6ca2f71dd5fd4
Rameez Siddiqui <157888573+rameez-sidd@users.noreply.github.com>
2024-10-31 20:34:32 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <header className='bg-slate-900 text-white py-3 px-12 text-center w-full flex items-center justify-between'>
         <div className='text-2xl'>&lt;Pass<span className='text-green-500 font-bold'>Wiz</span> /&gt;</div>
         <nav>
-            <a href="#" className='bg-green-500 flex items-center gap-2 px-2 py-1 rounded-full gitHub-btn'>
+            <a href="https://github.com/rameez-sidd/password-manager" target="_blank" className='bg-green-500 flex items-center gap-2 px-2 py-1 rounded-full gitHub-btn'>
               <span className='grid place-items-center'><FontAwesomeIcon style={{fontSize:'20px'}} icon={faGithub} /></span>
               <span className='font-semibold'>GitHub</span>
             </a>
